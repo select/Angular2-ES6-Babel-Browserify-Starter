fb9c51a4ff38d37f05bf4aa07de58a659731fca2
Fa!ko Krause <falko@webpgr.com>
2016-01-22 14:03:47 +0100

    not needed

--- server/config.js
+++
@@ -1,9 +0,0 @@
-let path = require('path');
-let environment = process.env.NODE_ENV || 'production';
-
-let localStaticPath = '../src';
-module.exports = {
-  env: environment,
-  port: process.env.PORT || 9000,
-  staticPath: path.resolve(__dirname, localStaticPath)
-};
